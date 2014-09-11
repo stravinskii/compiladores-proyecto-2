a070d54d1b66bc4324744b37067349587f55b3d8
Mauricio Gs <stravinskii@gmail.com>
2014-09-11 18:25:42 -0500

    Intento arreglar el merge

--- src/pylex.py
+++ src/pylex.py
@@ -1,5 +1,6 @@
-# this is a comment in python
-for x in xrange(1,10):
+# this is "string en comentario" a comment in python
+s = "Something string"
+for x in s:
 # is this a valid comment
 	x = True
 	while x:
@@ -23,3 +24,18 @@ while avanza: #comentario a media linea
 		print " esta es una cadenaa"
 		z = 10
 print "Termina el programa"
+
+
+num = 2
+num1 = 2 * 3
+num2 = 2 / 1
+num2 -= 1 
+while num < 11:
+	if num % 2 == 0:
+		print num , " es par"
+	else:
+		print num , " es impar"	
+	num += 1
+cadena = "Palabra"
+for c in cadena:
+    print c
