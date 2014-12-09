c7d516871a9b6e1fa18cd9b3d11e5c6ad178cb1f
stravinskii <stravinskii@gmail.com>
2014-12-09 01:50:54 -0600

    Nueva prueba de generación de código prueba.py

--- src/prueba.py
+++ src/prueba.py
@@ -1,10 +1,5 @@
-print "Hola mundo!"
-print 2 * 3 + 4 - 1
-x = 2
-y = 3
-x = x * y
-y = 4
-x = x + y
-y = 1
-x = x - y
-print x
+
+print "¡Hola Diana!, Este es un ejemplo de generación de código"
+x = 2 * 3 + 4 - 1
+print "Tenemos la suma: ", 2 * 3 + 4 - 1
+print "Mismo valor que se guardo en x: ", x
